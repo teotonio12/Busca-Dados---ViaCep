f27b99454463784a5133b07f4b1355202c7cd9b1
Renan Teotonio <73265007+teotonio12@users.noreply.github.com>
2020-12-31 16:10:14 -0400

    criando validação de CEP Valido

--- busca_dados.php
+++ busca_dados.php
@@ -1,5 +1,5 @@
 <?php 
-
+    //seta todas as variaveis zeradas para não dar erro, caso não tenha sido informada
     $resultado = (object) [
         'cep' => '',
         'logradouro' => '',
@@ -8,13 +8,32 @@
         'uf' => ''
     ];
 
-
+    //verifica se foi informado o cep
     if(isset ($_POST['cep'])){
+
+        //pega o valor informado
         $cep = $_POST['cep'];
 
-        $viacep = 'https://viacep.com.br/ws/'.$cep.'/json/';
+        //substitui tudo que não é numero  por vazio
+        $cep = preg_replace('/[^0-9]/','',$cep);
 
-        $resultado = json_decode(file_get_contents($viacep));
+        //verifica se contem 8 numeros separados ou nao por - , após a quinto caracter
+        if(preg_match('/^[0-9]{5}-?[0-9]{3}$/',$cep)){
+
+            //busca o endereço com a API ViaCep
+            $viacep = 'https://viacep.com.br/ws/'.$cep.'/json/';
+
+            //retorna os dados como um objeto
+            $resultado = json_decode(file_get_contents($viacep));
+
+        } else {//se não for um cep valido não realiza a consulta e os objetos continuam vazio
+            
+            //informa cep invalido
+            $resultado->cep = 'CEP inválido';
+        }
+        
+
+        
     }
 
 ?>
