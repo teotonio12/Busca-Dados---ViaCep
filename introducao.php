449ea6b7bd1ff20deeef9f1d5699ee4be3f53b66
Renan Teotonio <73265007+teotonio12@users.noreply.github.com>
2021-01-04 20:28:05 -0400

    add melhorias na introducao

--- introducao.php
+++ introducao.php
@@ -2,19 +2,33 @@
 
 
 <div class="container-fluid">
-  
+<h2>Introdução</h2>
   <div class="row">
     <div class="col-sm-4">
-        <h3>Introdução</h3>
+        <h3>Recomendação</h3>
+        <p>CEP - Conter 8 digitos, sem espaços e caracteres especiais.</p>
+        <p>UF - Apenas uf's brasileiros</p>
+        <p>Cidade e Logradouro - Conter no minimo 3 caracteres.</p>
+        <p></p>
     </div>
 
     <div class="col-sm-4">
         <h3>Busca Com Cep</h3>
+        <p>Para fazer a consulta utilizando o CEP.</p>
+        <p>Entre na Segunda aba "Busca Por CEP"</p>
+        <p>informe o CEP</p>
+        <p>click em Buscar</p>
+        <p>Aguarde o resultado aparecer abaixo.</p>
     </div>
 
     <div class="col-sm-4">
         <h3>Busca Com Dados</h3>
+        <p>Para fazer a consulta utilizando o UF-CIDADE-LOGRADOURO.</p>
+        <p>Entre na Terceira aba "Busca Por UF - Cidade"</p>
+        <p>informe os três campos</p>
+        <p>click em Buscar</p>
+        <p>Aguarde o resultado aparecer abaixo.</p>
     </div>
-    
+
   </div>
 </div>
