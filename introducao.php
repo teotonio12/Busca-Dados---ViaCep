428916cf0ebcdafae2bdbf978949e6ac2971dd2b
Renan Teotonio <73265007+teotonio12@users.noreply.github.com>
2021-01-04 20:18:22 -0400

    melhorando a introdução

--- introducao.php
+++ introducao.php
@@ -4,8 +4,17 @@
 <div class="container-fluid">
   
   <div class="row">
-    <div class="col-sm-4"><h3>Introdução</h3></div>
-    <div class="col-sm-4"><h3>Busca Com Cep</h3></div>
-    <div class="col-sm-4"><h3>Busca Com Dados</h3></div>
+    <div class="col-sm-4">
+        <h3>Introdução</h3>
+    </div>
+
+    <div class="col-sm-4">
+        <h3>Busca Com Cep</h3>
+    </div>
+
+    <div class="col-sm-4">
+        <h3>Busca Com Dados</h3>
+    </div>
+    
   </div>
 </div>
