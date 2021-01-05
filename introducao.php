eed1a50cbd660a84cc900b94230ff9dfba89f7ab
Renan Teotonio <73265007+teotonio12@users.noreply.github.com>
2021-01-04 20:17:24 -0400

    organazindo a introdução

--- introducao.php
+++ introducao.php
@@ -1,4 +1,11 @@
-<h3>Introdução</h3>
-    <p>Como Utilizar .</p>
-    <p>Instruções</p>
-    <p>Dúvidas</p>
+
+
+
+<div class="container-fluid">
+  
+  <div class="row">
+    <div class="col-sm-4"><h3>Introdução</h3></div>
+    <div class="col-sm-4"><h3>Busca Com Cep</h3></div>
+    <div class="col-sm-4"><h3>Busca Com Dados</h3></div>
+  </div>
+</div>
